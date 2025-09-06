a0c1e95ca05c1493d1457b4184697f285bc69e82
jdavidc <josedcolman@gmail.com>
2025-09-06 00:02:39 -0300

    Initial commit

---
+++ src/test/java/com/proxyapi/notifications/RealTimeNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxyapi.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
